33514232bbc17ce712b8ab5a78d8e93c97077037
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-09-19 23:31:29 +0300

    write the mockito test for the update status

--- src/main/java/com/linkedincourse/tddtesting/service/TaskService.java
+++ src/main/java/com/linkedincourse/tddtesting/service/TaskService.java
@@ -1,5 +1,31 @@
 package com.linkedincourse.tddtesting.service;
 
-public class TaskService {
+import java.util.Optional;
 
+import org.springframework.stereotype.Service;
+
+import com.linkedincourse.tddtesting.Task;
+import com.linkedincourse.tddtesting.TaskRepository;
+
+@Service
+public class TaskService {
+	
+	TaskRepository taskRepository;
+	
+	public TaskService(TaskRepository taskRepository) {
+		this.taskRepository = taskRepository;
+	}
+
+	public Task updateTaskStatus(Long id, String status) {
+		
+		Optional<Task> tOptional = taskRepository.findById(id);
+		
+		if (tOptional.isPresent()) {
+			Task t = tOptional.get();
+			t.setStatus(status);
+			return taskRepository.save(t);
+		}
+		
+		return null;
+	}
 }
--- src/test/java/com/linkedincourse/tddtesting/TaskServiceTest.java
+++ src/test/java/com/linkedincourse/tddtesting/TaskServiceTest.java
@@ -1,10 +1,19 @@
 package com.linkedincourse.tddtesting;
 
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.mockito.junit.jupiter.MockitoExtension;
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.timeout;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
+import static org.junit.jupiter.api.Assertions.*;
+
+import java.util.Optional;
 
 import com.linkedincourse.tddtesting.service.TaskService;
 
@@ -12,9 +21,26 @@ import com.linkedincourse.tddtesting.service.TaskService;
 public class TaskServiceTest {
 	
 	@Mock
-	TaskRepository taskRepository;
+	private TaskRepository taskRepository;
 	
 	@InjectMocks
-	TaskService taskService;
-	
+	private TaskService taskService;
+
+	@Test
+	void testUpdateTaskStatus() {
+		//init
+		Task task = new Task(1L, "Henry", "Developer");
+		
+		when(taskRepository.findById(1L)).thenReturn(Optional.of(task));
+		when(taskRepository.save(any(Task.class))).thenReturn(task);
+		
+		//logic
+		Task updatedTask = taskService.updateTaskStatus(1L, "In progress");
+		//assert
+		assertNotNull(updatedTask);
+		assertEquals("In progress", updatedTask.getStatus());
+		
+		verify(taskRepository, times(1)).findById(1L);
+		verify(taskRepository, timeout(1)).save(task);
+	}
 }
