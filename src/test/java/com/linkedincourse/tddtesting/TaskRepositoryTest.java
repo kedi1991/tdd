1eb180c10aaa2f5b4a513cf7ddd9f4cb46be84a9
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-09-19 22:21:52 +0300

    Add test to check all retrieved tasks

--- src/test/java/com/linkedincourse/tddtesting/TaskRepositoryTest.java
+++ src/test/java/com/linkedincourse/tddtesting/TaskRepositoryTest.java
@@ -6,6 +6,7 @@ import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.List;
 import java.util.Optional;
 
 @DataJpaTest
@@ -70,6 +71,25 @@ public class TaskRepositoryTest {
 		taskRepository.save(task);
 		//assert
 		assertEquals("Dealer", taskRepository.findById(task.getId()).get().getStatus());
+		
+	}
+	
+	@Test
+	void testFindAllTask() {
+		//init
+		Task task1 = new Task(null,"Cooper", "Unemployed");
+		taskRepository.save(task1);
+		
+		Task task2 = new Task(null,"Gilbert", "Unemployed");
+		taskRepository.save(task2);
+		
+		Task task3 = new Task(null,"Bob", "Unemployed");
+		taskRepository.save(task3);
+		
+		//logic
+		List<Task> allTasks = taskRepository.findAll();
+		//assert
+		assertEquals(3, allTasks.size());
 		
 	}
 	
