ebe81a6b21cff9201864e14cc3e79dd8ec880c9b
Okurut Henry Kedi <okurut.henry@yahoo.com>
2025-09-19 19:51:32 +0300

    Update the repository class to make it a proper interface

--- src/main/java/repository/TaskRepository.java
+++ src/main/java/repository/TaskRepository.java
@@ -1,10 +1,10 @@
 package repository;
 
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import jakarta.persistence.Id;
 import model.Task;
 
-public class TaskRepository {
+public interface TaskRepository extends JpaRepository<Long, Id>{
 
-	public Task save(Task task) {
-		return null;
-	}
 }
